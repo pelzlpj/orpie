c09fb903fe3d8893d49b7fd2cd7f6ac1a9c55a83
Paul Pelzl <pelzlpj@eecs.umich.edu>
2004-04-07 19:41:52 +0000

    added the prep-release script to prepare the sources for packaging

---
+++ prep-release.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# 'prep-release' script
+# Make all the last-minute changes to prepare the sources for packaging
+# in a release tarball
+
+echo "Removing {arch}/ ..."
+rm -rf "{arch}"
+echo "Generating ./configure ..."
+autoconf && rm -rf autom4te.cache
+echo "Creating documentation..."
+cd doc && make &> /dev/null
+echo "Done."
+
+# arch-tag: DO_NOT_CHANGE_f08da183-17c9-42c8-904b-7d6bab8dfe50 
