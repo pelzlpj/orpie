66aed8507eca8181eed1ffbb927f026dffe451d8
Paul Pelzl <pelzlpj@eecs.umich.edu>
2004-07-09 05:14:39 +0000

    implemented much faster base conversion algorithm for power-of-two bases big_ints can be represented in power-of-two bases very rapidly, because a time-consuming division can be replaced with relatively fast bit blits and shifts.  This represents a multiple order-of-magnitude time savings.

--- big_int_str.ml
+++ big_int_str.ml
@@ -62,18 +62,101 @@ let string_of_positive_int_base (num : int) (base : int) =
    get_digit num "" ""
 
 
-(* Divide-and-conquer algorithm for string representation of big_ints in
- * a desired base.  The big_int is split approximately in half using this
- * divisor:  base^(num_words * log_base(2^word_size) / 2)
- * Each half is split recursively, and the pieces are concatenated together.
- * Should run in O(n*log(n)) time, a big improvement on the standard O(n^2)
- * algorithm that requires one long division for every digit output. *)
+(* Divide a bignum by a power of 2, yielding a quotient and a remainder.
+ * Faster than general case division because it can be done using bit blitting 
+ * and shifting. *)
+let quomod_power_2 bignum bits =
+   let shift_words = bits / Sys.word_size
+   and shift_bits  = bits mod Sys.word_size in
+   let quot        = nat_of_big_int bignum in
+   let quot_len    = Nat.length_nat quot in
+   let rem         = Nat.create_nat (succ shift_words) in
+   (* copy the 'shift_words' least significant words of quot over to rem *)
+   Nat.blit_nat rem 0 quot 0 shift_words;
+   Nat.set_to_zero_nat rem shift_words 1;
+   (* shift over the remaining 'shift_bits' bits from quot to rem *)
+   Nat.shift_right_nat quot shift_words (quot_len - shift_words) rem shift_words
+   shift_bits;
+   (* adjust the most significant word of rem *)
+   let dummy = Nat.create_nat 1 in
+   Nat.shift_right_nat rem shift_words 1 dummy 0 (Sys.word_size - shift_bits);
+   let big_quot = big_int_of_nat (Nat.copy_nat quot shift_words (quot_len -
+   shift_words))
+   and big_rem  = big_int_of_nat rem in 
+   (big_quot, big_rem)
+   
+
+
+(* Divide-and-conquer algorithm for conversion to bases that are powers of two
+ * (binary, octal, and hex, for example).  The big_int is divided by a power
+ * of two that is also a power of 'base'; the power is chosen to approximately
+ * cut the big_int in half.  This division can be performed using only blits
+ * and bit shifts, so it is much faster than ordinary big_int division.
+ * The two pieces are recursively subdivided and the resulting strings are
+ * concatenated together. O(n*log(n)) time.  This function runs significantly
+ * faster than string_of_big_int_base_gen. *)
+let string_of_big_int_base (num : big_int) (base : int) =
+   let log2_base =
+      match base with
+      |2  -> 1
+      |4  -> 2
+      |8  -> 3
+      |16 -> 4
+      |32 -> 5
+      |_  ->
+         let err_str =
+            Printf.sprintf "Error: called string_of_big_int_base() with illegal base %d"
+            base
+         in
+         raise (Big_int_string_failure err_str)
+   in
+   let rec str_of_big_int_aux (ival : big_int) =
+      if is_int_big_int ival then
+         string_of_positive_int_base (int_of_big_int ival) base
+      else begin
+         let num_words = num_digits_big_int ival in
+         let half_binary_digits = num_words * Sys.word_size / 2 in
+         (* rounded_digits mod log2_base = 0, therefore
+          * division by (2^rounded_digits) will split ival
+          * perfectly with respect to the base 'base'. *)
+         let rounded_digits = half_binary_digits - 
+         (half_binary_digits mod log2_base) in
+         let upper, lower = quomod_power_2 ival rounded_digits in
+         let upper_string = str_of_big_int_aux upper
+         and lower_string = str_of_big_int_aux lower in
+         (* pad the lower_string with zeros as necessary *)
+         let adj_len_lower = rounded_digits / log2_base in
+         let zeros = 
+            String.make (adj_len_lower - (String.length lower_string)) '0'
+         in
+         upper_string ^ zeros ^ lower_string
+      end
+   in
+   let s = str_of_big_int_aux (abs_big_int num) in
+   match sign_big_int num with
+   |0    -> "0"
+   |1    -> s
+   |(-1) -> "-" ^ s
+   |x    -> 
+      raise (Big_int_string_failure ("unmatched sign: " ^ (string_of_int x)))
+
+
+
+
+
+(* Divide-and-conquer algorithm for string representation of big_ints in a
+* desired base (general case--not necessarily a power of two).  The big_int 
+* is split approximately in half using this divisor:  
+* base^(num_words * log_base(2^word_size) / 2) 
+* Each half is split recursively, and the pieces are concatenated together.
+* Should run in O(n*log(n)) time, a big improvement on the standard O(n^2)
+* algorithm that requires one long division for every digit output. *)
 (* Note 1: This runs in logarithmic stack space, so we should be able to handle
  * some pretty large big_ints before worrying about blowing the stack. *)
 (* Note 2: a faster method for computing a divisor would make this go a
  * lot quicker yet; gprof indicates that most of the time is spent in
  * multiplication ==> the power_int_positive_int_base call. *)
-let string_of_big_int_base (num : big_int) (base : int) =
+let string_of_big_int_base_gen (num : big_int) (base : int) =
    if base >= 2 && base <= 36 then
       let rec str_of_big_int_aux (ival : big_int) =
          if is_int_big_int ival then
@@ -101,6 +184,7 @@ let string_of_big_int_base (num : big_int) (base : int) =
          raise (Big_int_string_failure ("unmatched sign: " ^ (string_of_int x)))
    else
       raise (Big_int_string_failure ("unsupported base: " ^ (string_of_int base)))
+
 
 
 
