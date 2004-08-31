c277e25193ef71af496d9d585e0bc4d7b5134884
Paul Pelzl <pelzlpj@eecs.umich.edu>
2004-08-31 01:09:34 +0000

    improve the output messages in prep-release.sh

--- prep-release.sh
+++ prep-release.sh
@@ -3,7 +3,7 @@
 # Make all the last-minute changes to prepare the sources for packaging
 # in a release tarball
 
-echo "Loading curses bindings..."
+echo "Loading source dependencies..."
 tla build-config dist.arch
 echo "Removing {arch}/ ..."
 rm -rf "{arch}"
@@ -12,5 +12,8 @@ autoconf && rm -rf autom4te.cache
 echo "Creating documentation..."
 cd doc && make &> /dev/null
 echo "Done."
+
+
+
 
 # arch-tag: DO_NOT_CHANGE_f08da183-17c9-42c8-904b-7d6bab8dfe50 
